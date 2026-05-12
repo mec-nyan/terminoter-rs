b0cf0c5cfb0997b10e6940529fad17a96f00219b
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-05-12 20:55:45 -0300

    Basic Ratatui app.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> std::io::Result<()> {
+    ratatui::run(app)?;
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("rats!", frame.area());
 }
