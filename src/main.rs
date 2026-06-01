187d5f62d14c830017d6da50b56ae83e4b87bd76
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-01 18:16:09 -0300

    Parse command line arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use ratatui::{DefaultTerminal, Frame};
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Options {
+    // Optional file to read/write our notes.
+    file: Option<PathBuf>,
+}
+
+fn parse_args() -> Options {
+    return Options::parse();
+}
+
 fn main() -> std::io::Result<()> {
-    ratatui::run(app)?;
+    // Parse args + initial setup?
+    //
+    let opts = parse_args();
+
+    let path = opts.file.unwrap_or_default();
+
+    ratatui::run(|t| app(t, path.to_str().unwrap().to_string()))?;
     Ok(())
 }
 
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+fn app(terminal: &mut DefaultTerminal, path: String) -> std::io::Result<()> {
     loop {
-        terminal.draw(render)?;
+        terminal.draw(|f| {
+            render(f, path.clone());
+        })?;
         if crossterm::event::read()?.is_key_press() {
             break Ok(());
         }
     }
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("rats!", frame.area());
+fn render(frame: &mut Frame, path: String) {
+    frame.render_widget(path, frame.area());
 }
