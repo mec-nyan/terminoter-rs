6d612e3418bfbc2dfbeca6cac5efd80341618768
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2026-06-01 21:05:37 -0300

    Basic layout.

--- src/app.rs
+++ src/app.rs
@@ -2,7 +2,7 @@ use ratatui::{
     DefaultTerminal, Frame,
     layout::{Constraint, Layout},
     style::Style,
-    widgets::{Block, BorderType, Borders, Padding, Paragraph},
+    widgets::{Block, Borders, Padding, Paragraph, Wrap},
 };
 
 use crate::notes::Data;
@@ -19,27 +19,45 @@ pub fn app(terminal: &mut DefaultTerminal, data: Data) -> std::io::Result<()> {
 }
 
 fn render(frame: &mut Frame, data: &Data) {
-    if data.notes.len() == 0 {
-        frame.render_widget("Nothing to see here...", frame.area());
+    let main_pane = frame.area();
+
+    let layout = Layout::new(
+        ratatui::layout::Direction::Vertical,
+        vec![
+            Constraint::Length(3), // Title.
+            Constraint::Fill(1),   // Notes.
+        ],
+    )
+    .split(main_pane);
+
+    let title = Paragraph::new("== Notes ==")
+        .block(Block::new().padding(Padding::uniform(1)))
+        .centered();
+
+    frame.render_widget(title, layout[0]);
+
+    if data.notes.is_empty() {
+        frame.render_widget("Nothing to see here ...", layout[1]);
     } else {
-        let mut constraints: Vec<Constraint> = Vec::new();
-        for _ in 0..data.notes.len() {
-            constraints.push(Constraint::Min(1));
-        }
-        constraints.push(Constraint::Fill(2));
+        let constraints: Vec<Constraint> = vec![Constraint::Min(0); data.notes.len() + 1];
         let layout =
-            Layout::new(ratatui::layout::Direction::Vertical, constraints).split(frame.area());
+            Layout::new(ratatui::layout::Direction::Vertical, constraints).split(layout[1]);
 
         for i in 0..data.notes.len() {
-            let note = data.notes[i].clone();
-            let p = Paragraph::new(note.content)
+            let p = Paragraph::new(data.notes[i].content.clone())
                 .block(
                     Block::new()
+                        .padding(Padding {
+                            left: 1,
+                            right: 1,
+                            top: 0,
+                            bottom: 1,
+                        })
                         .borders(Borders::ALL)
-                        .border_type(BorderType::Rounded)
-                        .padding(Padding::new(2, 2, 1, 1)),
+                        .border_type(ratatui::widgets::BorderType::Rounded),
                 )
-                .style(Style::new().yellow());
+                .style(Style::new().yellow())
+                .wrap(Wrap { trim: true });
             frame.render_widget(p, layout[i]);
         }
     }
